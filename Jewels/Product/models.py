48193d0d8f9f46fafac34e2682734b5c375ab565
KidChaos <genesis.enedeh@gmail.com>
2021-08-08 09:27:19 +0100

    added a str method to my model

--- Jewels/Product/models.py
+++ Jewels/Product/models.py
@@ -6,3 +6,6 @@ class Jewelry_type(models.Model):
     name = models.CharField(max_length=200, unique=True)
     picture = models.ImageField(unique=True)
     description = models.TextField(unique=True)
+
+    def __str__(self):
+        return f"{Jewelry_type.name}"
