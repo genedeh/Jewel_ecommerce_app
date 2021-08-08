1bac88886501868091a1baa379537b232165c345
KidChaos <genesis.enedeh@gmail.com>
2021-08-08 09:17:46 +0100

    created a model called Jewelry_type and migrated

--- Jewels/Product/models.py
+++ Jewels/Product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Jewelry_type(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    picture = models.ImageField(unique=True)
+    description = models.TextField(unique=True)
