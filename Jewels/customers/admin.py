5bc8469fc3880b49d653c97a64408be01511f849
KidChaos <genesis.enedeh@gmail.com>
2021-10-22 18:15:26 +0100

    put in a models for customers

--- Jewels/customers/admin.py
+++ Jewels/customers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Order, OrderItem, WishList
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(WishList)
--- Jewels/customers/models.py
+++ Jewels/customers/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from Product.models import Jewelry
+
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    image = models.ImageField(blank=True, default='/no_profile_image.jpg')
+    password = models.CharField(max_length=8, unique=True)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, blank=True, null=True)
+    wishlist = models.ForeignKey("WishList", on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=180)
+    order_item = models.ManyToManyField('OrderItem')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Jewelry, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'OrderItem Product: {self.product}'
+
+
+class WishList(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    jewelry = models.ManyToManyField(Jewelry)
+
+    def __str__(self):
+        return f'{self.name}'
