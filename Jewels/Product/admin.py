461e52ae3093bb82e733d76f90c34389c5f9b1d1
KidChaos <genesis.enedeh@gmail.com>
2021-10-22 17:51:48 +0100

    put in admin decorations

--- Jewels/Product/admin.py
+++ Jewels/Product/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Jewelry_type, Jewelry
+
+
 # Register your models here.
-admin.site.register(Jewelry_type)
-admin.site.register(Jewelry)
+class JewelryTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'picture', 'description']
+    search_fields = ['name']
+    list_display_links = ['name']
+    list_editable = ['picture']
+
+
+class JewelryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'image', 'jewelry_type', 'video', 'star']
+    list_display_links = ['name']
+    list_editable = ['image', 'video']
+    search_fields = ['name', 'star']
+
+
+admin.site.register(Jewelry_type, JewelryTypeAdmin)
+admin.site.register(Jewelry, JewelryAdmin)
+
+# headers and titles
+admin.site.site_header = 'JEWELS ADMIN PANEL'
+admin.site.site_title = 'JEWELRY SHOP SITE ADMIN'
+admin.site.index_title = 'JEWELRY DATA ADMINISTRATION'
--- Jewels/Product/models.py
+++ Jewels/Product/models.py
@@ -13,7 +13,7 @@ class Jewelry_type(models.Model):
 
 class Jewelry(models.Model):
     name = models.CharField(max_length=250, unique=True)
-    jewelry_type = models.ForeignKey(Jewelry_type, on_delete=models.PROTECT, default=1)
+    jewelry_type = models.ForeignKey(Jewelry_type, on_delete=models.PROTECT, default=1, related_name='jewelries')
     image = models.ImageField(default='/No product image.png')
     video = models.FileField(blank=True)
     description = models.TextField()
