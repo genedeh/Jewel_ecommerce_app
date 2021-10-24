86ffe66c7551fbc6a2e71114f8f1ebb30c79c083
KidChaos <genesis.enedeh@gmail.com>
2021-10-24 22:00:03 +0100

    added query select

--- Jewels/Product/views.py
+++ Jewels/Product/views.py
@@ -5,5 +5,5 @@ from .models import Jewelry_type, Jewelry
 # Create your views here.
 def home_view(request):
     jewelry_types = Jewelry_type.objects.all().order_by("name")
-    jewelries = Jewelry.objects.all().order_by("name")
+    jewelries = Jewelry.objects.filter(star__gt=2).order_by("name")[:6]
     return render(request, 'home.html', {'jewelry_types': jewelry_types, 'jewelries': jewelries})
